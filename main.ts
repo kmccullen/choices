509e06a6ca9ec3172f872da6e0acacb4715be6c9
Kevin McCullen <kmccullen@users.noreply.github.com>
2020-09-25 09:59:07 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,31 @@
+let Item = 0
+input.onGesture(Gesture.Shake, function () {
+    Item = randint(1, 3)
+    if (Item == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (Item == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . . . .
+            . # . . .
+            . . . . .
+            `)
+    } else if (Item == 3) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . .
+            `)
+    } else {
+    	
+    }
+})
